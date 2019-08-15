7c950ae587d998a69406697a039b7fb3b1426ede
Praveen Mathew <praveen.mathew92@outlook.com>
2019-08-15 19:07:40 +0530

    Add notes for each Interface

--- src/PublisherImpl.java
+++ src/PublisherImpl.java
@@ -1,9 +1,26 @@
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 public class PublisherImpl implements Publisher {
+    /*
+            dont start without subscribe() -> onSubscribe() -> initialization logic can be executed by the Subscriber
+
+            can invoke onNext of Subscriber.
+            onNext on subscriber <= Subscription.elementCount
+            end with onComplete and onError
+
+            fail -> onError
+
+            onComplete|onError -> Subscriber's Subscription -> cancel()
+
+            stop signalling after subscription cancelled
+     */
+
     @Override
     public void subscribe(Subscriber subscriber) {
+        Subscription subscription = new SubscriptionImpl(this, subscriber);
+        subscriber.onSubscribe(subscription);
 
     }
 }
--- src/SubscriberImpl.java
+++ src/SubscriberImpl.java
@@ -2,9 +2,24 @@ import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
 public class SubscriberImpl implements Subscriber {
+    /*
+            signal demand -> supscription.request()
+            decide when and how many elements
+
+            cancel subscriptions no longer needed
+     */
+    private static final int BUFFER_SIZE = 128;
+    private boolean terminalState;
+
+    public SubscriberImpl() {
+        this.terminalState = false;
+    }
+
     @Override
     public void onSubscribe(Subscription subscription) {
+        // cancel active subscription
 
+        subscription.request(BUFFER_SIZE);
     }
 
     @Override
@@ -14,11 +29,18 @@ public class SubscriberImpl implements Subscriber {
 
     @Override
     public void onError(Throwable throwable) {
+        //not call publisher or subscription
+        //cancel subscription
 
+        terminalState = true;
     }
 
     @Override
     public void onComplete() {
+        //not call publisher or subscription
+        //cancel subscription
 
+
+        terminalState = true;
     }
 }
