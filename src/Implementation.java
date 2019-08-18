a2f21fe28298ad390b2373215c61cdda50502541
Praveen Mathew <praveen.mathew92@outlook.com>
2019-08-18 23:15:22 +0530

    Publish natural numbers indefinately

--- src/Implementation.java
+++ src/Implementation.java
@@ -1,9 +1,13 @@
-import Subscription.SubscribeInteger;
+import Subscriber.SubscribeInteger;
+import publisher.NaturalNumbersPublisher;
 import publisher.SingleIntegerPublisher;
 
 public class Implementation {
     public static void main(String[] args) {
         SingleIntegerPublisher publisher = new SingleIntegerPublisher(5);
         publisher.subscribe(new SubscribeInteger());
+
+        NaturalNumbersPublisher naturalNumbersPublisher = new NaturalNumbersPublisher();
+        naturalNumbersPublisher.subscribe(new SubscribeInteger());
     }
 }
---
+++ src/publisher/NaturalNumbersPublisher.java
@@ -0,0 +1,39 @@
+package publisher;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+import java.util.Iterator;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+
+public class NaturalNumbersPublisher implements Publisher<Integer>{
+    private final Supplier<IntStream> naturalNumbersSupplier = () -> IntStream.iterate(1, i -> i + 1);
+
+    @Override
+    public void subscribe(Subscriber<? super Integer> subscriber) {
+        subscriber.onSubscribe(new Subscription(subscriber));
+    }
+
+    class Subscription implements org.reactivestreams.Subscription {
+        private final Subscriber subscriber;
+        private final Iterator iterator;
+
+        Subscription(Subscriber subscriber) {
+            this.subscriber = subscriber;
+            this.iterator = naturalNumbersSupplier.get().iterator();
+        }
+
+        @Override
+        public void request(long elementCount) {
+            while(iterator.hasNext()){
+                subscriber.onNext(iterator.next());
+            }
+        }
+
+        @Override
+        public void cancel() {
+
+        }
+    }
+}
